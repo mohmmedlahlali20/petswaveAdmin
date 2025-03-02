b9ae6c211287209dcf5843667edc561fcd0e093f
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-02 06:05:12 +0000

    fix(api): enhance user API service integration

--- src/redux/service/api/user.ts
+++ src/redux/service/api/user.ts
@@ -3,6 +3,9 @@ import path from "../../../axios/path.ts";
 
 const loginApi = async ({email, password}:{email:string, password: string}) => {
     const  login = await path.post('auth/login', {email, password})
+    console.log('====================================');
+    console.log(login);
+    console.log('====================================');
     return login.data
 }
 
