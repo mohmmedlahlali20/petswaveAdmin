8c860f7689ab7bb652d00b980d04957268e4aab4
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-02 10:44:16 +0000

    feat(redux): add pet slice for managing pet data

---
+++ src/redux/Slice/petSlice.ts
@@ -0,0 +1,84 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Pets } from "../../constant/type";
+import { addPetsApi, getPetsApi } from "../service/api/pets";
+
+
+
+
+
+const initialState: {
+    isLoading: boolean,
+    error: string | null,
+    pets: Pets[],
+    petSelected: Pets | null,
+
+
+} = {
+    isLoading: false,
+    error: null,
+    pets: [],
+    petSelected: null
+}
+
+
+export const addPets = createAsyncThunk(
+    "pets/add",
+    async (petData: Pets, { rejectWithValue }) => {
+        try {
+            return await addPetsApi(petData);
+        } catch (err: any) {
+            return rejectWithValue(err.message);
+        }
+    }
+)
+
+export const getPets = createAsyncThunk('pets/get',
+    async (_, { rejectWithValue }) => {
+        try {
+            return await getPetsApi()
+
+        } catch (err: any) {
+            return rejectWithValue(err.message);
+
+        }
+    })
+
+
+
+const petSlice = createSlice({
+    name: "Pets",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(addPets.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(addPets.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.pets.push(action.payload);
+            })
+            .addCase(addPets.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload as string;
+            })
+            .addCase(getPets.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(getPets.fulfilled, (state, action: PayloadAction<Pets[]>) => {
+                state.pets = action.payload;
+                state.isLoading = false;
+                state.error = null;
+            })
+            .addCase(getPets.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload as string;
+            })
+    }
+});
+
+
+
+export default petSlice.reducer
